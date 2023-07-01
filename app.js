308f55362e95f276233552a913c83d355b5ef627
Abhinand <psabhinand333s@gmail.com>
2023-07-02 01:34:59 +0530

    prod: cors

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const cors = require('cors');
+
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./Controllers/errorController");
